06765962cd34fc2322437b671808a38e8ff845b0
bugtype <bugtype.kr@gmail.com>
2019-02-27 10:45:18 +0900

    Updated image opacity animation

--- src/containers/LeftBodyArea.js
+++ src/containers/LeftBodyArea.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
+import './LeftBodyArea.css';
 
 
 const mockDeviceImageUrl = "https://www.instagram.com/static/images/homepage/home-phones.png/43cc71bb1b43.png"
 
-const images = [
-  "https://www.instagram.com/static/images/homepage/screenshot2-2x.jpg/177140221987.jpg",
-  "https://www.instagram.com/static/images/homepage/screenshot1-2x.jpg/9144d6673849.jpg"
-]
+
 const style = {
   backgroundImage: `url(${mockDeviceImageUrl})`,
   backgroundSize: '454px 618px;',
@@ -23,14 +21,38 @@ const imageStyle = {
 const dummyTopStyle = {
   width: 304,
 }
-
 export default class LeftBodyArea extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCount: 0,
+      images: [
+        "https://www.instagram.com/static/images/homepage/screenshot2-2x.jpg/177140221987.jpg",
+        "https://www.instagram.com/static/images/homepage/screenshot1-2x.jpg/9144d6673849.jpg"
+      ]
+  };
+
+  }
+  timer() {
+    this.setState({
+      currentCount: this.state.images.length - 1 == this.state.currentCount ? 0 : this.state.currentCount + 1
+    })
+  }
+  componentDidMount(){
+    this.intervalId = setInterval(this.timer.bind(this), 2000);
+  }
+  componentWillUnmount(){
+    clearInterval(this.intervalId);
+  }
   render() {
     return (
       <div style={style}>
       <div style={dummyTopStyle} />
-        <img src={images[0]} style={imageStyle} />
-        <img src={images[1]} style={imageStyle} />
+      {
+        this.state.images.map((imageUrl, i) => {
+                        return i == this.state.currentCount ? (<img src={imageUrl} className="thumbnailImage" />) : (<img src={imageUrl} className="thumbnailImage hide" />);
+                    })
+      }
       </div>
     )
   }
